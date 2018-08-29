9cfca7940f8792eac02c95d39daa7b6085a2df8a
Bekpasha Dursunov <32161488+thisisbeka@users.noreply.github.com>
2018-08-29 21:34:30 +0300

    Update ex1.c

--- week2/ex1.c
+++ week2/ex1.c
@@ -1,1 +1,12 @@
-
+// Preprocessor directives requesting to include the necessary header files
+#include <stdio.h>
+#include <limits.h>
+#include <float.h>
+int main(int argc, char *argv[]) {
+ double z = DBL_MAX;  // assign the values  
+ float y = FLT_MAX; 
+ int x = INT_MAX;          
+  printf("Integer size: %lu\nFloat size: %lu\nDouble size: %lu\n", sizeof(x), sizeof(y), sizeof(z)); // print the values 
+  printf("Max integer: %d\nMaximum float: %f\nMaximum double: %lf\n", x, y, z); // print the values 
+  return 0;
+}
