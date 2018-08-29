2bddaf473c2f84abbf2c79aa193121c26891db61
Bekpasha Dursunov <32161488+thisisbeka@users.noreply.github.com>
2018-08-29 22:03:54 +0300

    Update ex3.c

--- week2/ex3.c
+++ week2/ex3.c
@@ -1,1 +1,18 @@
-
+#include <stdio.h>
+int main(void) {
+  int s;
+  scanf("%d", &s);
+  for (int i=0; i<s; i++) {
+    for (int k=0; k<(s-i-1); k++) {
+      printf(" ");
+    }
+    for (int j=0; j<=(2*i); j++) {
+      printf("*");
+    }
+    for (int z=0; z<(s-i); z++) {
+      printf(" ");
+    }
+     printf("\n");
+  }
+  return 0;
+}
