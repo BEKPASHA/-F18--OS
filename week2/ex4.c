38603b0b3825d60c69174358a59c27d420fbf91c
Bekpasha Dursunov <32161488+thisisbeka@users.noreply.github.com>
2018-08-29 22:10:33 +0300

    Update ex4.c

--- week2/ex4.c
+++ week2/ex4.c
@@ -1,1 +1,15 @@
+#include <stdio.h>
 
+int main(int argc, char *argv[]) {
+    int *a, *b, x, y;
+    scanf("%d%d", &x, &y);
+    a = &x;
+    b = &y;
+    swapInt(a, b);
+    printf("After swapping, x is %d and y is %d", x, y);
+}
+void swapInt(int *a, int *b) {
+    int temp = *a;
+    *a = *b;
+    *b = temp;
+}
