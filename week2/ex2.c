a937560bd7fb82a2200cfb7d7c0d80215655659f
Bekpasha Dursunov <32161488+thisisbeka@users.noreply.github.com>
2018-08-29 21:45:07 +0300

    Update ex2.c

--- week2/ex2.c
+++ week2/ex2.c
@@ -1,1 +1,19 @@
+#include <stdio.h>
+#include <string.h>
 
+int main(int argc, char *argv[]) {
+  char Str[100];
+
+  scanf("%s", Str);
+  
+  char temp;   
+  int str_size = strlen(Str);   
+  for(int i=0; i<str_size/2; i++) {
+    temp = Str[str_size-1-i];
+    Str[str_size-1-i] = Str[i];
+    Str[i] = temp;
+  }
+
+  puts(Str);
+  return 0;
+}
