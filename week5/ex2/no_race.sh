3b95c9859c5b2d04b202c194eddc794eb50e0a19
Bekpasha Dursunov <32161488+thisisbeka@users.noreply.github.com>
2018-09-19 23:13:29 +0300

    Update no_race.sh

--- week5/ex2/no_race.sh
+++ week5/ex2/no_race.sh
@@ -1,1 +1,16 @@
+if test ! -f 12
+then
+    echo 0 > 12
+fi
 
+if ln 12 12.lock
+	then                                                                    
+	for i in `seq 1 100`;
+	do
+		endnum=`tail -1 12`
+		endnum=$((endnum+1))
+
+		echo $endnum >> 12
+	done
+	rm 12.lock
+fi
