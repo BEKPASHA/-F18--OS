3cb3fde095066416ec2ecd6d50f887c3669dadca
Bekpasha Dursunov <32161488+thisisbeka@users.noreply.github.com>
2018-09-19 23:07:49 +0300

    Update ex1.c

--- week5/ex1.c
+++ week5/ex1.c
@@ -1,1 +1,19 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <pthread.h> 
+void *hi_thread(void *vargp) {
+    printf("Hi thread #%d\n", *((int*) vargp));
+    pthread_exit(NULL);
+}
+int main() {
+    int n;
+    scanf("%d", &n);
+    int i;
+    for (i = 0; i < n; ++i) {
+	printf("Running thread #%d\n", i);
 
+	pthread_t thread_id;
+        pthread_create(&thread_id, NULL, hi_thread, (void*) &i);
+	pthread_join(thread_id, NULL); 
+    }
+}
