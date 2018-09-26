ff1837fd7bd47ec1c0218e34f8646a120ea1b2c5
Bekpasha Dursunov <32161488+thisisbeka@users.noreply.github.com>
2018-09-26 18:41:02 +0300

    Update ex5.c

--- week6/ex5/ex5.c
+++ week6/ex5/ex5.c
@@ -1,1 +1,23 @@
+#include <stdio.h>
+#include <signal.h>
+#include <unistd.h>
+#include <stdlib.h>
 
+int main (void) {
+	
+	pid_t pid = fork();
+
+	if(pid == 0){
+		// if we are in child process
+		while(1){
+			printf("I’m alive\n");
+			sleep(1);
+		}
+	} else{
+		// if we are in parent process
+		sleep(10);
+		printf("Parent process sends SIGTERM signal to child\n");
+		kill(pid, SIGTERM);
+	}	
+	return EXIT_SUCCESS;
+}
