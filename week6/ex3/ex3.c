2f86286fe844e06a13f185fe6d72ba5ceb886dbc
Bekpasha Dursunov <32161488+thisisbeka@users.noreply.github.com>
2018-09-26 18:24:04 +0300

    Update ex3.c

--- week6/ex3/ex3.c
+++ week6/ex3/ex3.c
@@ -1,1 +1,20 @@
+#include <stdio.h>
+#include <signal.h>
+#include <unistd.h>
+#include <stdlib.h>
 
+void sig_handler (int signo) {
+	/* your signal handler code */
+	printf("\nExit program\n");
+	exit(EXIT_SUCCESS);
+}
+
+int main (void) {
+	/* set signal handler here */
+	/* what for signal from user */
+	while (1){
+ 		signal(SIGINT, sig_handler);
+ 		sleep (1);
+	}
+ 
+	
