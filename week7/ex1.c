e315de779ba872a0f1820704f76f68c83b039b4b
Bekpasha Dursunov <32161488+thisisbeka@users.noreply.github.com>
2018-10-03 19:29:17 +0300

    Update ex1.c

--- week7/ex1.c
+++ week7/ex1.c
@@ -1,1 +1,22 @@
+#include <stdio.h>
+#include <unistd.h>
+#include <string.h>
+#include <sys/types.h>
 
+int main(){
+
+	int fd[2];
+	char string[] = "Hello, world!\n";
+	char buffer[100];
+
+	pipe(fd);
+
+	write(fd[1], string, (strlen(string) + 1));
+	close(fd[1]);
+	read(fd[0], buffer, sizeof(buffer));
+	close(fd[0]);
+	printf("Received string: %s", buffer);
+
+
+	return 0;
+}
