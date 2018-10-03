8eeff0c77c4bc3c56b525a697ef38d4295a1dbe0
Bekpasha Dursunov <32161488+thisisbeka@users.noreply.github.com>
2018-10-03 19:30:49 +0300

    Update ex2.c

--- week7/ex2.c
+++ week7/ex2.c
@@ -1,1 +1,18 @@
+#include <stdio.h>
+#include <stdlib.h>
 
+int main(){
+
+	printf("Enter number of elements in array: \n");
+	int N;
+	scanf("%d", &N);
+	int *array = malloc(N * sizeof(int));
+	int i;
+	for (i = 0; i < N; i++){
+		array[i] = i;
+		printf("%d ", array[i]);
+	}
+	printf("\n");
+	free(array);
+	return 0;
+}
